211270c9f798443a902b2081c6e6320f58fdeef5
Omar <omaaraly7@gmail.com>
2023-05-20 15:09:25 +0300

    Added Headless part with phantom JS

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,6 +3,9 @@ package tests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
@@ -16,6 +19,7 @@ import utilities.Helper;
 public class TestBase extends AbstractTestNGCucumberTests {
 public static WebDriver driver;
 
+@SuppressWarnings("deprecation")
 @BeforeSuite
 @Parameters({"Browser"})
 public void startDriver(@Optional("chrome") String BrowserName)
@@ -28,6 +32,15 @@ public void startDriver(@Optional("chrome") String BrowserName)
 	{
 		//System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+ "/Drivers/geckodriver.exe");
 		driver = new FirefoxDriver();
+	} else if (BrowserName.equalsIgnoreCase("headless"))
+	{
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setJavascriptEnabled(true);
+		caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, 
+				"C://Users//Lenovo//Documents//Eclipse_Github_Clonetest//SeleniumFramework//Drivers//phantomjs.exe");
+		String [] PhantomArgs = {"--web-security=no","--ignore-ssl-errors=yes"};
+		caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, PhantomArgs);
+		driver = new PhantomJSDriver(caps);
 	}
 	driver.navigate().to("https://demo.nopcommerce.com/");
 	
